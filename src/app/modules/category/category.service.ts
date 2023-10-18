e536f01a8933ec2f8301492115f8bef7bab031b3
mitaly-dev <mitalyy.dev@gmail.com>
2023-10-18 20:27:40 +0600

    category service created

---
+++ src/app/modules/category/category.service.ts
@@ -0,0 +1,52 @@
+import { Category } from '@prisma/client';
+import { prisma } from '../../../shared/prisma';
+
+const insertIntoDB = async (data: Category): Promise<Category> => {
+  const result = await prisma.category.create({ data });
+  return result;
+};
+
+const getAllData = async (): Promise<Category[]> => {
+  const result = await prisma.category.findMany({});
+
+  return result;
+};
+
+const getData = async (id: string): Promise<Category | null> => {
+  const result = await prisma.category.findUnique({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+const updateData = async (
+  id: string,
+  payload: Partial<Category>,
+): Promise<Category> => {
+  const result = await prisma.category.update({
+    where: {
+      id,
+    },
+    data: payload,
+  });
+  return result;
+};
+
+const deleteData = async (id: string): Promise<Category> => {
+  const result = await prisma.category.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+export const CategoryService = {
+  insertIntoDB,
+  getAllData,
+  getData,
+  updateData,
+  deleteData,
+};
