7cbdfa0fd9c317d3a939a46e39b133ad5c439a6a
mitaly-dev <mitalyy.dev@gmail.com>
2023-10-18 20:14:39 +0600

    booking interface created

---
+++ src/app/modules/Booking/booking.interface.ts
@@ -0,0 +1,11 @@
+export type IfilterData = {
+  search?: string;
+  studentId?: string;
+  firstName?: string;
+  middleName?: string;
+  email?: string;
+  contactNo?: string;
+  gender?: string;
+  bloodGroup?: string;
+  academicSemeserId?: string;
+};
